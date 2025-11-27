c7d925b41c6f80e8dba9364da91e7a342dc4fbf4
OhtoGangg <ogealaviiva@gmail.com>
2025-11-27 18:20:10 +0200

    Update keepalive.js

--- keepalive.js
+++ keepalive.js
@@ -1,1 +1,8 @@
-ggg
+const express = require("express");
+const app = express();
+
+app.all("/", (req, res) => {
+  res.send("Bot is running!");
+});
+
+app.listen(3000, () => console.log("Botin pitäisi nyt pysyä hereillä!"));
